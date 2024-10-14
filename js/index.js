87827a794f45e66578f6d9bb38501d05a9d23f05
Angshuman Gupta <angshu.gupta789@gmail.com>
2024-10-14 21:18:43 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Nothing to see here :D
+document.addEventListener('DOMContentLoaded', function() {
+        const playButton = document.getElementById('playButton');
+        const audio = document.getElementById('careerPodcast');
+
+        playButton.addEventListener('click', function() {
+          if (audio.paused) {
+            audio.play();
+            playButton.textContent = 'Pause';
+          } else {
+            audio.pause();
+            playButton.textContent = 'Play';
+          }
+        });
+
+        audio.addEventListener('ended', function() {
+          playButton.textContent = 'Play';
+        });
+      });
