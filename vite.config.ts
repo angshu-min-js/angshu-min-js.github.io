857c2aa029dff0b528e0d0255c9e3a1ea83c3072
angshu-min-js <angshu.gupta789@gmail.com>
2025-04-21 19:45:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: '/',
   server: {
     host: "::",
@@ -27,4 +27,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
